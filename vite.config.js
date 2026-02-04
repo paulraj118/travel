c36feb9766a602400097872d1dbbfe537c35cbb1
paulraj118 <paulraj112003@gmail.com>
2026-02-04 16:52:49 +0530

    modify

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base:'/tourist-visitor/',
   plugins: [react()],
-})
+  base: "/tourist-visitor/"
+});
+                                                                                
