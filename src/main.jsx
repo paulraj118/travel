19084cb0fd37b0d3feab3d970c87cd6fc1383674
paulraj118 <paulraj112003@gmail.com>
2026-02-04 16:27:26 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/tourist-visitor">
+    <BrowserRouter basename="/tourist-visitor/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
