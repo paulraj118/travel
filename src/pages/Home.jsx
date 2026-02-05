e6b1bab845618002172353d64f891e3cbd38d3b0
paulraj118 <paulraj112003@gmail.com>
2026-02-05 15:44:36 +0530

    modify

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
+import bgVideo from "../assets/vedio.mp4";
 
 function Home() {
   return (
     <div className="home-container">
-      <video className="bg-video" autoPlay loop muted playsInline>
-        <source src="/vedio.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
 
-      <div className="nav-buttons">
+      {/* Background Video */}
+    <video className="bg-video" autoPlay muted loop>
+  <source src={bgVideo} type="video/mp4" />
+</video>
+
+
+      {/* Top Navigation */}
+      <div className="nav-buttons top">
         <Link to="/"><button>Home</button></Link>
         <Link to="/about"><button>About</button></Link>
         <Link to="/services"><button>Services</button></Link>
         <Link to="/contact"><button>Contact Us</button></Link>
       </div>
 
+      {/* Center Content */}
       <div className="content">
         <h1>Welcome to My Website</h1>
         <p>Explore the beauty 🌿</p>
       </div>
+
     </div>
   );
 }
